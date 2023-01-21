1841bb53b8a4d71f40cc2bc52910096780ef011b
Ahmet Karagöz <61436772+AhmetKaragozz@users.noreply.github.com>
2023-01-21 22:08:50 +0300

    Add files via upload

---
+++ BasicLogin/BasicLogin/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogin
+{
+    class BasicLogin
+    {
+        static void Main(string[] args)
+        {
+
+            string correctUsername = "admin";
+            string correctPassword = "password";
+
+
+            int loginAttempts = 3;
+
+
+            Console.WriteLine("Please enter your username and password to login:");
+
+
+            while (loginAttempts > 0)
+            {
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+
+                if (username == correctUsername && password == correctPassword)
+                {
+
+                    Console.WriteLine("Login is successful. Welcome!");
+                    break;
+                }
+                else
+                {
+                    // The login was unsuccessful
+                    loginAttempts--;
+                    Console.WriteLine("Username or password wrong. " + loginAttempts + " attempts remain.");
+                }
+            }
+
+            // If the user has run out of login attempts, exit the program
+            if (loginAttempts == 0)
+            {
+                Console.WriteLine("Program is shuttingdown.");
+            }
+        }
+    }
+}
